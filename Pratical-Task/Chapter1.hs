8f7176cb6f09a1689e3706be4bf06f3020c35344
muhdmm000-beep <muhdmm000@gmail.com>
2025-10-16 11:15:33 +0100

    Update Chapter1.hs

--- Pratical-Task/Chapter1.hs
+++ Pratical-Task/Chapter1.hs
@@ -1,1 +1,13 @@
+double :: Int -> Int
+double x = x * 2 
 
+increment :: Int -> Int 
+increment x = x + 1
+ 
+doubleThenIncrement :: Int -> Int
+doubleThenIncrement = increment . double
+
+main :: IO ()
+main = do 
+ print $ doubleThenIncrement 3
+ print $ doubleThenIncrement 5
